ded742d7500f6d4ea39caf781bbf29d7e9ebf9be
sda97ghb <sda97g@gmail.com>
2017-04-20 21:59:49 +0500

    Добавлены структуры для часов и минут вместо векторов и кортежей.

--- StringTime.cpp
+++ StringTime.cpp
@@ -1,12 +1,18 @@
 #include "StringTime.h"
 
-#include <tuple>
 #include <vector>
 
-namespace
+namespace fuzzy_time
 {
+    struct Hour
+    {
+        std::wstring name;
+        std::wstring nextName;
+        std::wstring nextGenetive;
+    };
+
     /// @brief Названия часов.
-    std::vector<std::vector<std::wstring>> hoursStrings =
+    std::vector<Hour> hoursStrings =
     {
         //          текущий         следующий    следующий в родительном падеже
         /*00    */ {L"полночь",     L"час",         L"первого"      },
@@ -35,63 +41,84 @@ namespace
 
     /// @brief Cтруктура для записей соответсятвий минут.
     /// @details Время начала (мин, сек), название минут, порядок вывода.
-    using minutesRecord = std::tuple<short, short, std::wstring, TimeOrder>;
+    struct Minutes
+    {
+        short minutes;
+        short seconds;
+        std::wstring name;
+        TimeOrder order;
+    };
 
     /// @brief Таблица соответствия минут.
-    std::vector<minutesRecord> minutesStrings = {
+    std::vector<Minutes> minutesStrings = {
         // время начала(м, с) строка вывода          порядок вывода
-        minutesRecord(57, 30, L"почти ровно",         TimeOrder::nextEnd),
-        minutesRecord(52, 30, L"без пяти",            TimeOrder::nextEnd),
-        minutesRecord(47, 30, L"без десяти",          TimeOrder::nextEnd),
-        minutesRecord(42, 30, L"без четверти",        TimeOrder::nextEnd),
-        minutesRecord(37, 30, L"без двадцати",        TimeOrder::nextEnd),
-        minutesRecord(32, 30, L"без двадцати пяти",   TimeOrder::nextEnd),
-        minutesRecord(27, 30, L"пол",                 TimeOrder::nextGenetive),
-        minutesRecord(22, 30, L"двадцать пять минут", TimeOrder::nextGenetive),
-        minutesRecord(17, 30, L"двадцать минут",      TimeOrder::nextGenetive),
-        minutesRecord(12, 30, L"с четвертью",         TimeOrder::current),
-        minutesRecord( 7, 30, L"десять минут",        TimeOrder::nextGenetive),
-        minutesRecord( 2, 30, L"пять минут",          TimeOrder::nextGenetive),
-        minutesRecord( 0,  0, L"ровно",               TimeOrder::current)
+        {57, 30, L"почти ровно",         TimeOrder::nextEnd},
+        {52, 30, L"без пяти",            TimeOrder::nextEnd},
+        {47, 30, L"без десяти",          TimeOrder::nextEnd},
+        {42, 30, L"без четверти",        TimeOrder::nextEnd},
+        {37, 30, L"без двадцати",        TimeOrder::nextEnd},
+        {32, 30, L"без двадцати пяти",   TimeOrder::nextEnd},
+        {27, 30, L"пол",                 TimeOrder::nextGenetive},
+        {22, 30, L"двадцать пять минут", TimeOrder::nextGenetive},
+        {17, 30, L"двадцать минут",      TimeOrder::nextGenetive},
+        {12, 30, L"с четвертью",         TimeOrder::current},
+        {7, 30, L"десять минут",        TimeOrder::nextGenetive},
+        {2, 30, L"пять минут",          TimeOrder::nextGenetive},
+        {0,  0, L"ровно",               TimeOrder::current}
     };
     const std::wstring s0015 = L"пятнадцать минут первого";
 }
 
 /// @brief Определение, находится ли время после указанного.
-bool OverTime(char minutes, char seconds, char minMinutes, char minSeconds) {
-    return !(minutes < minMinutes || (minutes == minMinutes && seconds < minSeconds));
+bool TimeIsGreater(char minutes1, char seconds1, char minutes2, char seconds2)
+{
+    if (minutes1 > minutes2)
+        return true;
+    if (minutes1 == minutes2 && seconds1 > seconds2)
+        return true;
+    return false;
 }
 
 /// @brief Генерация строки минут.
-std::pair<std::wstring, TimeOrder> GenMinutes(char minutes, char seconds) {
-    for(unsigned i = 0; i < minutesStrings.size(); ++ i)
-        if (OverTime(minutes, seconds, std::get<0>(minutesStrings.at(i)), std::get<1>(minutesStrings.at(i))))
-            return std::pair<std::wstring, TimeOrder>(std::get<2>(minutesStrings.at(i)), std::get<3>(minutesStrings.at(i)));
-    return std::pair<std::wstring, TimeOrder>(L"MINUTESERROR", TimeOrder::current);
+std::pair<std::wstring, fuzzy_time::TimeOrder>
+GenMinutes(char minutes, char seconds)
+{
+    using fuzzy_time::minutesStrings;
+    using fuzzy_time::TimeOrder;
+    for (unsigned i = 0; i < minutesStrings.size(); ++ i)
+        if (TimeIsGreater(minutes, seconds, minutesStrings.at(i).minutes, minutesStrings.at(i).seconds))
+            return std::make_pair(minutesStrings.at(i).name, minutesStrings.at(i).order);
+    return std::make_pair(L"MINUTESERROR", TimeOrder::current);
 }
 
 /// @brief Генерация строки часа.
-std::wstring GenHour(char hour, TimeOrder timeOrder) {
+std::wstring GenHour(char hour, fuzzy_time::TimeOrder timeOrder) {
+    using fuzzy_time::hoursStrings;
+    using fuzzy_time::TimeOrder;
+
     if (hour > 12) hour -= 12;
     if (hour < 0 || hour > 12) return L"HOURERROR";
     switch (timeOrder) {
-        case TimeOrder::current:      return hoursStrings[hour][0]; break;
-        case TimeOrder::nextBegin:    return hoursStrings[hour][1]; break;
-        case TimeOrder::nextEnd:      return hoursStrings[hour][1]; break;
-        case TimeOrder::nextGenetive: return hoursStrings[hour][2]; break;
+        case TimeOrder::current:      return hoursStrings[hour].name; break;
+        case TimeOrder::nextBegin:    return hoursStrings[hour].nextName; break;
+        case TimeOrder::nextEnd:      return hoursStrings[hour].nextName; break;
+        case TimeOrder::nextGenetive: return hoursStrings[hour].nextGenetive; break;
     };
     return L"HOURERROR";
 }
 
 /// @brief Генерация времени словами.
 std::wstring timeToString(int hour, int minutes, int seconds){
+    using fuzzy_time::hoursStrings;
+    using fuzzy_time::minutesStrings;
+    using fuzzy_time::TimeOrder;
     std::pair<std::wstring, TimeOrder> a = GenMinutes(minutes, seconds);
     std::wstring m = a.first;
     std::wstring h = GenHour(hour, a.second);
-    if ((h == hoursStrings[0][0] || h == hoursStrings[12][0]) && // ровно в полночь и полдень
-         m == std::get<2>(minutesStrings.at(12))) return h;           // не нужно выводить минуты
-    if (h == hoursStrings[0][0] && m == std::get<2>(minutesStrings.at(9)))
-        return s0015;                                           //в 00:15 выводится особая строка
+    if ((h == hoursStrings[0].name || h == hoursStrings[12].name) && // ровно в полночь и полдень
+         m == minutesStrings.at(12).name) return h;          // не нужно выводить минуты
+    if (h == hoursStrings[0].name && m == minutesStrings.at(9).name)
+        return fuzzy_time::s0015;                                           //в 00:15 выводится особая строка
     switch (a.second) {
         case TimeOrder::current:      return h + L" " + m;
         case TimeOrder::nextBegin:    return h + L" " + m;
